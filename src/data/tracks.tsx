ff11b418e38f173a4750e834f715958065586091
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-29 16:27:34 -0400

    Process Page fixes

--- src/data/tracks.tsx
+++ src/data/tracks.tsx
@@ -14,17 +14,16 @@ export const tracks: Track[] = [
     imgUrl: "images/tracks/campus-circuit.png",
     background: `bg-[url(images/tracks/campus-circuit.png)]`,
     name: "Campus Circuit",
-    description:
-      `This Outer Loop is around RIT campus, you may have driven around here as if you drive on campus this loops is used very often,
+    description: `This Outer Loop is around RIT campus, you may have driven around here as if you drive on campus this loops is used very often,
        if you are ready to finally be able to ignore the stop signs and be ready to rev your engine and speed.`,
     devDescription: (
-      <div className="flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col md:flex-row">
         <img
           src="images/tracks/dev-campus-circuit.jpg"
           alt="Outer Loop Sketch"
           className="md:max-w-60 lg:max-w-[20vw]"
         />
-        <p className="md:ml-10 text-center md:text-left">
+        <p className="md:ml-10 text-center md:text-left text-body">
           Campus Circuit is inspired by RIT's "Outer Loop," a large road that
           encircles the entire campus. As such, the track features multiple
           landmarks around campus, such as the Dorm buildings and the Gene
@@ -43,7 +42,12 @@ export const tracks: Track[] = [
       "Remember the days you used to live in the dorm? well if you wanna relive it with a better memory you can, speed around the dorm in tiny karts. if you still live in the dormside or will be living there be ready to live this experience and dont forget to clean your room otherwise little characters might drive around it.",
     devDescription: (
       <div className="flex flex-col md:flex-row">
-        <p className="md:mr-10 text-center md:text-left">
+        <img
+          src="images/tracks/dev-dorm-derby.jpg"
+          alt="Dorm Room Sketch"
+          className="md:max-w-60 lg:max-w-[20vw]"
+        />
+        <p className="md:ml-10 text-center md:text-left text-body">
           The Dorm Room Derby is inspired by RIT's dorm rooms, where students
           make their second homes. Inspired by Mario Kart's Ribbon Road, the
           track was given a clean side and a messy side to its visuals,
@@ -51,11 +55,6 @@ export const tracks: Track[] = [
           track is of intermediate difficulty, testing players that have a basic
           familiarity with the game's mechanics.
         </p>
-        <img
-          src="images/tracks/dev-dorm-derby.jpg"
-          alt="Dorm Room Sketch"
-          className="md:max-w-60 lg:max-w-[20vw]"
-        />
       </div>
     ),
   },
@@ -66,13 +65,13 @@ export const tracks: Track[] = [
     description:
       "Have you ever wanted to go around golisano, well you can now. going past classrooms, up and down the floors.",
     devDescription: (
-      <div className="flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col md:flex-row">
         <img
           src="images/tracks/dev-tech-house.jpg"
           alt="Tech House Sketch"
           className="md:max-w-60 lg:max-w-[20vw]"
         />
-        <p className="md:ml-10">
+        <p className="md:ml-10 text-body">
           Tech House Turnpike is based on Golisano Hall, the primary building
           where many classes for Game Design majors are held. The primary appeal
           of the track is its vertically structured layout, split into 3
@@ -91,7 +90,12 @@ export const tracks: Track[] = [
       do an average rit student event known as an all nighter.`,
     devDescription: (
       <div className="flex flex-col md:flex-row">
-        <p className="md:mr-10">
+        <img
+          src="images/tracks/dev-all-nighter.jpg"
+          alt="Final Brick Road Sketch"
+          className="md:max-w-60 lg:max-w-[20vw]"
+        />
+        <p className="md:ml-10 text-body">
           All-Nighter Expressway is heavily inspired by Mario Kart's Rainbow
           Road, in both aesthetics and challenge level. The track is set in
           nighttime to distinguish itself from the other tracks, which occur in
@@ -100,11 +104,6 @@ export const tracks: Track[] = [
           eponymous practice, All-Nighter Expressway is a final test of skill,
           encompassing the difficulty of all tracks before it.
         </p>
-        <img
-          src="images/tracks/dev-all-nighter.jpg"
-          alt="Final Brick Road Sketch"
-          className="md:max-w-60 lg:max-w-[20vw]"
-        />
       </div>
     ),
   },
--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -15,7 +15,7 @@ const ProcessPage = () => {
 
   return (
     <>
-      <main className="mt-0 px-6 md:px-12 relative bg-[url(images/items-background.png)] bg-[#BBB] bg-fixed bg-repeat bg-blend-difference *:text-white">
+      <main className="mt-0 px-6 md:px-12 relative bg-[url(images/items-background.png)] bg-[#BBB] bg-fixed bg-repeat bg-blend-difference bg-size-[90%] md:bg-size-[80%] *:text-white">
         {/* About Project Section */}
         <div>
           <h1 className="text-header2 pt-4 mx-0 mb-4"> About the Project</h1>
@@ -27,36 +27,38 @@ const ProcessPage = () => {
               students are split into 8 sub-teams by their specialty:
               <ul className="list-disc *:my-2 ml-5">
                 <li>
-                  <b>Production:</b> Oversees the entire team's work, assigning
-                  tasks and handling marketing.
+                  <b className="text-xl">Production:</b> Oversees the entire
+                  team's work, assigning tasks and handling marketing.
                 </li>
                 <li>
-                  <b>Gameplay and Systems Programming:</b> Programs all of the
-                  game's features and mechanics in Unity.
+                  <b className="text-xl">Gameplay and Systems Programming:</b>{" "}
+                  Programs all of the game's features and mechanics in Unity.
                 </li>
                 <li>
-                  <b>Web Development:</b> Programs the game's website,
-                  summarizing information and displaying game statistics.
+                  <b className="text-xl">Web Development:</b> Programs the
+                  game's website, summarizing information and displaying game
+                  statistics.
                 </li>
                 <li>
-                  <b>Level Design:</b> Designs and iterates on the layouts and
-                  themes of the game's tracks.
+                  <b className="text-xl">Level Design:</b> Designs and iterates
+                  on the layouts and themes of the game's tracks.
                 </li>
                 <li>
-                  <b>Content Development:</b> Creates all the game's art assets
-                  and intellectual property.
+                  <b className="text-xl">Content Development:</b> Creates all
+                  the game's art assets and intellectual property.
                 </li>
                 <li>
-                  <b>Quality Assurance:</b> Ensures code and documentation is
-                  well-organized through frequent testing.
+                  <b className="text-xl">Quality Assurance:</b> Ensures code and
+                  documentation is well-organized through frequent testing.
                 </li>
                 <li>
-                  <b>Support:</b> Assists other teams in various areas and
-                  ensures progress doesn't slow down.
+                  <b className="text-xl">Support:</b> Assists other teams in
+                  various areas and ensures progress doesn't slow down.
                 </li>
                 <li>
-                  <b>Research &amp; Development:</b> Researches construction of
-                  the game's arcade cabinet and potential features.
+                  <b className="text-xl">Research &amp; Development:</b>{" "}
+                  Researches construction of the game's arcade cabinet and
+                  potential features.
                 </li>
               </ul>
               The project and most of its students work in the school of
@@ -184,9 +186,7 @@ const ProcessPage = () => {
         </div>
 
         {/* Track Dev Info Section */}
-        <h1 className="text-[40px] poppins m-4 mx-0 mb-4">
-          Building our Tracks
-        </h1>
+        <h1 className="text-header2 m-4 mx-0 mb-4">Building our Tracks</h1>
         <p className="mb-8 text-body">
           The tracks of Gizmo Go-Kartz are largely based on real RIT locations
           with original twists. With an initial plan of 4 tracks per cup and
@@ -197,7 +197,6 @@ const ProcessPage = () => {
           added as the finishing touch. In addition, track layouts are tweaked
           in response to feedback from playtesters.
         </p>
-        <h1 className="text-header2 m-4 mx-0 mb-4">Building our Tracks</h1>
         <Tabs defaultValue="0" className="w-[90vw]">
           <TabsList className="flex flex-wrap h-full pl-0">
             {tracks.map((t) => {
@@ -223,64 +222,66 @@ const ProcessPage = () => {
           })}
           <TabsContent value="cut">
             <div className="bg-linear-to-b from-[#F66624] to-[#D84B3A] p-5 rounded-e-2xl rounded-b-2xl mb-3 *:mb-5">
-              <div className="flex flex-col md:flex-row">
-                <div className="md:mr-10">
-                  <h1 className="text-3xl font-bold poppins mb-3">
-                    Quarter-Mile
-                  </h1>
-                  <p className="text-body">
-                    A track based on RIT's Quarter Mile, a long pathway
-                    connecting the dorm buildings to the academic buildings, was
-                    developed and tested several times, but eventually cut from
-                    the track roster. The layout's size and length made it
-                    highly challenging to scale down to the level of a track
-                    with 3 laps.
-                  </p>
+              <div className="max-h-[52vh] overflow-y-auto">
+                <div className="flex flex-col md:flex-row">
+                  <div className="md:mr-10">
+                    <h1 className="text-3xl font-bold poppins mb-3">
+                      Quarter-Mile
+                    </h1>
+                    <p className="text-body">
+                      A track based on RIT's Quarter Mile, a long pathway
+                      connecting the dorm buildings to the academic buildings,
+                      was developed and tested several times, but eventually cut
+                      from the track roster. The layout's size and length made
+                      it highly challenging to scale down to the level of a
+                      track with 3 laps.
+                    </p>
+                  </div>
+                  <img
+                    src="images/tracks/dev-quarter-mile.jpg"
+                    alt="Quarter Mile Sketch"
+                    className="md:max-w-80 lg:max-w-[25vw]"
+                  />
                 </div>
-                <img
-                  src="images/tracks/dev-quarter-mile.jpg"
-                  alt="Quarter Mile Sketch"
-                  className="md:max-w-80 lg:max-w-[25vw]"
-                />
-              </div>
-              <div className="flex flex-col md:flex-row-reverse">
-                <div className="md:ml-10">
-                  <h1 className="text-3xl font-bold poppins mb-3">
-                    Global Village
-                  </h1>
-                  <p className="text-body">
-                    A track based on RIT's Global Village was drafted early into
-                    development, going through several proposed layouts that
-                    emphasized several branching pathways. However, there was no
-                    concrete plan for the track, and it ultimately was not
-                    modeled or incorporated into the roster.
-                  </p>
+                <div className="flex flex-col md:flex-row-reverse">
+                  <div className="md:ml-10">
+                    <h1 className="text-3xl font-bold poppins mb-3">
+                      Global Village
+                    </h1>
+                    <p className="text-body">
+                      A track based on RIT's Global Village was drafted early
+                      into development, going through several proposed layouts
+                      that emphasized several branching pathways. However, there
+                      was no concrete plan for the track, and it ultimately was
+                      not modeled or incorporated into the roster.
+                    </p>
+                  </div>
+                  <img
+                    src="images/tracks/dev-global-village.jpg"
+                    alt="Global Village Sketch"
+                    className="md:max-w-40 lg:max-w-[20vw]"
+                  />
                 </div>
-                <img
-                  src="images/tracks/dev-global-village.jpg"
-                  alt="Global Village Sketch"
-                  className="md:max-w-40 lg:max-w-[20vw]"
-                />
-              </div>
-              <div>
-                <h1 className="text-3xl font-bold poppins mb-3">
-                  Other Cut Track Concepts
-                </h1>
-                <ul className="list-disc ml-5 *:mb-2 text-body">
-                  <li>
-                    <b>The SHED:</b> An early drafted track based on the Student
-                    Hall for Exploration and Development (SHED). The track was
-                    never given a proper layout and cut for similar reasons to
-                    the Quarter Mile.
-                  </li>
-                  <li>
-                    <b>RIT Woods:</b> A track was modeled and visualized based
-                    on the woods near Grace Watson Hall to contrast with the
-                    urban visuals of the other tracks. The design ended up not
-                    aligning thematically or feeling recognizable as an RIT
-                    location, and thus was cut.
-                  </li>
-                </ul>
+                <div>
+                  <h1 className="text-3xl font-bold poppins mb-3">
+                    Other Cut Track Concepts
+                  </h1>
+                  <ul className="list-disc ml-5 *:mb-2 text-body">
+                    <li>
+                      <b>The SHED:</b> An early drafted track based on the
+                      Student Hall for Exploration and Development (SHED). The
+                      track was never given a proper layout and cut for similar
+                      reasons to the Quarter Mile.
+                    </li>
+                    <li>
+                      <b>RIT Woods:</b> A track was modeled and visualized based
+                      on the woods near Grace Watson Hall to contrast with the
+                      urban visuals of the other tracks. The design ended up not
+                      aligning thematically or feeling recognizable as an RIT
+                      location, and thus was cut.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </TabsContent>
