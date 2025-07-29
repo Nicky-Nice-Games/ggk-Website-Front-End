4a906ba2f4b5b6ab35d2c3ea3230da39c486699f
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-29 15:35:08 -0400

    change news header font

--- src/components/home/home-carousel.tsx
+++ src/components/home/home-carousel.tsx
@@ -26,7 +26,7 @@ export const HomeCarousel = () => {
           <img className="h-[100%]" src={image}></img>
         </div>
         <div>
-          <h2 className="text-[40px] md:text-[60px] bebas">{header}</h2>
+          <h2 className="text-[40px] md:text-[60px] racing">{header}</h2>
           <p className="text-[18px]">{description}</p>
         </div>
       </div>
