79d32fa879e4c4ac802456fa9cffd0f36a8d7d97
eviKS23 <eks2733@rit.edu>
2025-08-01 09:34:03 -0400

    Updated MapSelect layout, fixed some of the banner borders, improved responsiveness

--- src/data/items.ts
+++ src/data/items.ts
@@ -11,61 +11,61 @@ export interface Item {
 export const items: Item[] = [
   {
     imgUrl: "images/items/boosts/level_one.png",
-    name: "Rocket Boost",
+    name: "TigerClaw",
     level: 1,
     category: "boost",
-    description: "increase your speed by 100% for 3 seconds",
+    description: "This energy drink is quite familiar to students on campus. It gives the player a 1.25x boost to their speed for three seconds!",
   },
   {
     imgUrl: "images/items/boosts/level_two.png",
-    name: "Double Rocket Boost",
+    name: "FizzyClaw",
     level: 2,
     category: "boost",
-    description: "increase your speed by 200% for 3 seconds",
+    description: "This alternate flavor of TigerClaw gives a LOT more energy... It gives the player a 1.5x boost to their speed for three seconds!",
   },
   {
     imgUrl: "images/items/boosts/level_three.png",
-    name: "Flying Rocket Boost",
+    name: "RocketClaw",
     level: 3,
     category: "boost",
     description:
-      "increase your speed by 300% for 3 seconds, lifts character up in the air",
+      "Do you... even drink them at this point? RocketClaw gives the player a 1.75x boost to their speed for three seconds, but also makes them airborne, allowing for some cool tricks!",
   },
   {
     imgUrl: "images/items/boosts/level_four.png",
-    name: "Upgraded Boost",
+    name: "WarpClaw",
     level: 4,
     category: "boost",
     description:
-      "jump through a warp portal and move your kart up 3 map checkpoints",
+      "Cracking open a WarpClaw teleports the player quite a distance on the course, and then leaves them with a TigerClaw boost. I wonder how many Dining Dollars these cost?",
   },
   {
     imgUrl: "images/items/shields/level_one.png",
-    name: "4 Second Shield",
+    name: "Shield",
     level: 1,
     category: "defense",
-    description: "Become invulnerable for 4 seconds",
+    description: "This item generates a force field that will protect the user for four seconds.",
   },
   {
     imgUrl: "images/items/shields/level_two.png",
-    name: "6 Second Shield",
+    name: "Tough Shield",
     level: 2,
     category: "defense",
-    description: "Become invulnerable for 6 seconds",
+    description: "This upgraded version of the shield protects the user for six seconds.",
   },
   {
     imgUrl: "images/items/shields/level_three.png",
-    name: "8 Second Shield",
+    name: "Tougher Shield",
     level: 3,
     category: "defense",
-    description: "Become invulnerable for 8 seconds",
+    description: "This shield protects the user for eight seconds. Surely it can't get better than that...",
   },
   {
     imgUrl: "images/items/shields/level_four.png",
-    name: "10 Second Shield",
+    name: "Toughest Shield",
     level: 4,
     category: "defense",
-    description: "Become invulnerable for 10 seconds",
+    description: "Whoa! This shield protects the user for ten seconds, but also stuns people that the user bumps into. I guess the best defense really IS a good offense...",
   },
   {
     imgUrl: "images/items/pucks/level_one.png",
@@ -73,23 +73,23 @@ export const items: Item[] = [
     level: 1,
     category: "offense",
     description:
-      "A tier one (base) level offensive projective, If player is hit with a puck, they are slowed down",
+      "A clean, offensive item you can throw in a straight line, to slow down an opponent if it lands. It even bounces off of walls!",
   },
   {
     imgUrl: "images/items/pucks/level_two.png",
-    name: "Homing Spikey Puck",
+    name: "Homing Puck",
     level: 2,
     category: "offense",
     description:
-      "A tier two level offensive projective, Will auto go to next player ahead and hit them",
+      "This upgraded version of the puck chases after the next player in front of you. No need to worry about aiming!",
   },
   {
     imgUrl: "images/items/pucks/level_three.png",
-    name: "Triple Homing Pucks",
+    name: "Spiky Pucks",
     level: 3,
     category: "offense",
     description:
-      "A tier three level offensive projective, will use three pucks with the same features of Homing Spikey Puck",
+      "What's better than one homing puck? Three of them! They've even got spikes now for an added OOMPH!",
   },
   {
     imgUrl: "images/items/pucks/level_four.png",
@@ -97,7 +97,7 @@ export const items: Item[] = [
     level: 4,
     category: "offense",
     description:
-      "A level four (Final/Max) offensive projective, will go attack the player in first",
+      "This puck chases down whichever player is currently in first place, and then swoops down and hits them. That's just cruel...",
   },
   {
     imgUrl: "images/items/fake/level_one.png",
