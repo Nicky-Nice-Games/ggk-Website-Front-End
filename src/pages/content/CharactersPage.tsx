8173c23d6fa1808eb87629887b2816d61f3faaae
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-08-04 10:35:40 -0400

    close icon fixed

--- src/pages/content/CharactersPage.tsx
+++ src/pages/content/CharactersPage.tsx
@@ -29,37 +29,41 @@ const CharactersPage = () => {
           {/* Character carousel */}
           <CharacterCarousel
             contentClass="lg:max-w-240 bg-[#0000] border-none shadow-none"
+            showDialogClose={false}
             mappedContent={(character) => {
               return (
                 <>
-                <div className="flex flex-col justify-center h-[100%] relative">
-                  <div
-                    className="absolute md:h-[150%] lg:h-[170%] 2xl:h-[200%] z-10 
+                  <div className="flex flex-col justify-center h-[100%] relative">
+                    <div
+                      className="absolute md:h-[150%] lg:h-[170%] 2xl:h-[200%] z-10 
                     md:-translate-x-[50%] lg:-translate-x-[15%] 2xl:-translate-x-[30%]"
-                  >
-                    <img
-                      src={character.imgUrl}
-                      alt={character.name}
-                      className="md:h-[100%] hidden md:block"
-                    />
-                  </div>
-                  <div className="w-full bg-gray-200 rounded-lg drop-shadow-xl/50">
-                    {/* right side: character name and description */}
-                    <DialogDescription className=" p-[2rem] md:ml-[8rem] lg:ml-[4rem] 2xl:ml-[7rem] text-left">
-                      <div className="text-left md:w-7/10 md:ml-auto h-full">
-                        <div className="flex flex-row justify-between mb-2">
-                          <DialogTitle className="text-black font-bold text-header2 justify-self-start">
-                            {character.name}
-                          </DialogTitle>
+                    >
+                      <img
+                        src={character.imgUrl}
+                        alt={character.name}
+                        className="md:h-[100%] hidden md:block"
+                      />
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-lg drop-shadow-xl/50">
+                      {/* right side: character name and description */}
+                      <DialogDescription className=" p-[2rem] md:ml-[8rem] lg:ml-[4rem] 2xl:ml-[7rem] text-left">
+                        <div className="text-left md:w-7/10 md:ml-auto h-full">
+                          <div className="flex flex-row justify-between mb-2">
+                            <DialogTitle className="text-black font-bold text-header2 justify-self-start">
+                              {character.name}
+                            </DialogTitle>
+                          </div>
+                          <p className="text-[#262626] text-body whitespace-pre-line h-[85%] flex flex-col justify-center">
+                            {character.description}
+                          </p>
                         </div>
-                        <p className="text-[#262626] text-body whitespace-pre-line h-[85%] flex flex-col justify-center">
-                          {character.description}
-                        </p>
-                      </div>
-                    </DialogDescription>
+                      </DialogDescription>
+                      {/*Readded to position properly*/}
+                      <DialogClose className="absolute top-4 right-4">
+                        <CloseIcon />
+                      </DialogClose>
+                    </div>
                   </div>
-
-                </div>
                 </>
               );
             }}
