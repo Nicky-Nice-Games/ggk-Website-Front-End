4705aa924ae2588135f2d20422ec687344b9a23b
Ztw4322 <ztw4322@rit.edu>
2025-08-06 15:09:57 -0400

    home/news and updates

--- src/pages/NewsAndUpdates.tsx
+++ src/pages/NewsAndUpdates.tsx
@@ -102,7 +102,7 @@ const NewsAndUpdatesPage = () => {
             <motion.div
               layoutId={`item-${active.title}-${id}`}
               ref={ref}
-              className={`w-18/20 h-[90%] md:h-150 md:max-h-[70%] bg-white dark:bg-neutral-900 rounded-3xl overflow-hidden ${isMobile
+              className={`w-18/20 h-[90%] md:h-150 md:max-h-[70%] bg-white  rounded-3xl overflow-hidden ${isMobile
                   ? "flex flex-col overflow-y-auto"
                   : "flex flex-col md:flex-row"
                 }`}
